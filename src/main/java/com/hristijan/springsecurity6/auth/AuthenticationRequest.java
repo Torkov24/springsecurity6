4c57e9f02bb648e483974b77aa837829265cb54a
Hristijan Torkov <hristijan_to5@hotmail.com>
2023-09-29 14:51:30 +0200

    Added complete files

---
+++ src/main/java/com/hristijan/springsecurity6/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.hristijan.springsecurity6.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
